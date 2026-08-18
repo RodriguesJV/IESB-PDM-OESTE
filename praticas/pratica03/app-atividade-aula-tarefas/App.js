13068b7cd82f66f8a433fe404a6a5ad97c856944
RodriguesJV <joaovit3625@gmail.com>
2026-08-17 21:30:11 -0300

    feat:a4- positions e dimensões do app

---
+++ praticas/pratica03/app-atividade-aula-tarefas/App.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { rotulo_btn_cadastro_meta, rotulo_input_meta, rotulo_lista_metas } from './mensagens';
+
+
+export default function App() {
+  return (
+    <View style={styles.mainContainer}>
+      <View style={{flexDirection:'row', justifyContent:'space-between', flex:1}}>
+        <View style={{width:'65%'}}>
+          <TextInput style = {styles.inputText} placeholder={rotulo_input_meta} />
+        </View> 
+        <View style={{width:'30%'}}>
+          <Button title={rotulo_btn_cadastro_meta} />
+        </View> 
+      </View>      
+      <View style={styles.metaContainer}>
+        <Text>{rotulo_lista_metas}</Text> 
+      </View> 
+        
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainContainer:{
+    padding: 30,
+    flex:1,
+    flexDirection:'column'
+  },
+  inputText:{
+    borderColor:'#cccccc',
+    borderWidth:1
+  },
+  metaContainer:{
+    flex:1
+  }
+});
